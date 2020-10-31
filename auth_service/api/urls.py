e4be13203b141e0ae60bce46459d9d1ff3979b37
isuru <isurumahesh97@gmail.com>
2020-10-31 00:23:21 -0700

    added signin view

--- auth_service/api/urls.py
+++ auth_service/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from oauth2_provider.views import TokenView
+
 from .views import UserSignupView
 
 app_name = "api"
 urlpatterns = [
     path('sign-up/', UserSignupView.as_view(), name="sign-up"),
+    path('sign-in/', TokenView.as_view(), name="sign-in"),
 ]
